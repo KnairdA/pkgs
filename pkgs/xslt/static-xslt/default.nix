bdc4a57c8da0b0c12b22579c5ecff1bb495945b3
Adrian Kummerlaender <knairda@t-online.de>
2018-11-13 19:07:45 +0100

    Bump static-xslt version

--- pkgs/xslt/static-xslt/default.nix
+++ pkgs/xslt/static-xslt/default.nix
@@ -5,9 +5,9 @@ stdenv.mkDerivation rec {
 
   src = fetchFromGitHub {
     owner = "KnairdA";
-    repo = "StaticXSLT";
-    rev = "master";
-    sha256 = "17gd181cw9yyc4h1fn7fikcgm8g7fdwm7d7fxwib4aynm18kwqad";
+    repo  = "StaticXSLT";
+    rev    = "0b84f519cb418d32c0fd62f9c6c852ee31f7c926";
+    sha256 = "058kld9ysagzama0l6hnn6v6mm6jf29fbf78q0l92fxl0mlabd7n";
   };
 
   installPhase = ''
