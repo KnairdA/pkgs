7aacf581bc43da8633989f553f38b1689d5a7724
Adrian Kummerlaender <adrian@kummerlaender.eu>
2020-03-22 21:22:10 +0100

    Use writeShellScriptBin trivial builder for nix-shell wrapper
    Thanks to @jobojeha for the suggestion

--- pkgs/persistent-nix-shell/default.nix
+++ pkgs/persistent-nix-shell/default.nix
@@ -1,9 +1,5 @@
 { stdenv, pkgs, ... }:
 
-pkgs.writeTextFile {
-  name        = "persistent-nix-shell";
-  executable  = true;
-  destination = "/bin/persistent-nix-shell";
-
-  text = builtins.readFile ./persistent-nix-shell.sh;
-}
+pkgs.writeShellScriptBin
+  "persistent-nix-shell"
+  (builtins.readFile ./persistent-nix-shell.sh)
