9a55c6a1dd53b2851ead64c9941e5f881704456f
Adrian Kummerlaender <adrian@kummerlaender.eu>
2020-04-25 20:48:40 +0200

    Bump InputXSLT version

--- pkgs/xslt/input-xslt/default.nix
+++ pkgs/xslt/input-xslt/default.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation rec {
     owner = "KnairdA";
     repo = "InputXSLT";
     rev = "master";
-    sha256 = "1pjhq1jjfdhd0k3fckvir465l34wkbvqp47r7r6qdd93fg6kw3x8";
+    sha256 = "1p2y1m65h1k3g0nj8sgc05f9w880wngrd2n1b53dhhqq4scsj2a7";
   };
 
   buildInputs = [ cmake boost xalanc xercesc discount ];
